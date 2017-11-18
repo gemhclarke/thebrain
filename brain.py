0beeb9318e2ef1fb39709ccd139ba4b08a6a6b3c
Gem Clarke <gemhclarke@gmail.com>
2017-11-18 17:09:36 +0000

    broken threads

--- brain.py
+++ brain.py
@@ -5,7 +5,8 @@ from blinkstick import blinkstick
 from time import sleep
 bstick1 = blinkstick.find_first()
 from mpu6050 import mpu6050
-from threading import Thread
+#from threading import Thread
+import threading
 import pygame
 import os
 
@@ -15,8 +16,9 @@ import os
 # ===================================================================
 
 def leds_reset():
+
+    print("Resetting LEDs")
     for led_number in range(8):
-	print("Resetting LEDs")
         bstick1.set_color( index=led_number, red=0, green=0, blue=0)
 
 
@@ -27,11 +29,11 @@ def leds_morph( colour, brightness ):
 
     for led_number in range(8):
         r,g,b = adjust_brightness( colour, brightness )
-	#print("Setting led " + str(led_number) + " to HEX: " + colour + " RGB: "+ str(r) + " " + str(g) + " " + str(b) )
-        #print ("Gyro data is: " + str(gyro_x) + " " + str(gyro_y) + " " + str(gyro_z) )
 
         if( (gyro_x > 30 or gyro_x < -30) or (gyro_y > 30 or gyro_y < -30) or (gyro_z > 30 or gyro_x < -30) ):
-            print ("Emergency")
+            print ("Emergency - gyroscope movement detected")
+            emergencySoundThread = threading.Thread(target=play_emergency_sound)
+            emergencySoundThread.start()
             leds_gyro_emergency()
 
         bstick1.morph( index=led_number, red=r, green=g, blue=b, duration=400, steps=30)
@@ -39,9 +41,8 @@ def leds_morph( colour, brightness ):
 
 def leds_gyro_emergency():
 
-    # Start the sound thread 
-    st = Thread(target=play_emergency_sound)
-    st.start()
+    # Start the emergency LED pattern. We'll repeat 5 cycles but each time check the value of gyro_x and gyro_y
+    # If these value are back in range we declare the emergency is over and reload the background sound and break the loop
 
     for count in range(5):
         for led_number in range(8):
@@ -53,23 +54,27 @@ def leds_gyro_emergency():
             bstick1.morph( index=led_number, red=100, green=0, blue=0, duration=200, steps=30)
 
         if( (gyro_x < 30 and gyro_x > -30) and (gyro_y < 30 and gyro_y > -30) and (gyro_z < 30 and gyro_x > -30) ):
-            print (">>>>>>>> Emergency over, reverting back to normal mode")
+            print ("Emergency over, reverting back to normal mode")
+	    backgroundSoundThread = threading.Thread( target=play_background_sound, args=() )
+            backgroundSoundThread.start()
             break  
 
 
 def adjust_brightness( colour, brightness ):
-    #
+     
     # This function converts HEX to RGB and reduces each RGB value by the
     # same amount to act as a brightness control
     # The brightness argument can be anything from 0.1 to 255.0
-    #
+     
     r,g,b = bstick1._hex_to_rgb( colour ) 
     r = r * (brightness/100)
     g = g * (brightness/100)
     b = b * (brightness/100)
     return(r,g,b)
 
+
 def play_emergency_sound():
+    print("Playing emergency sound")
     pygame.mixer.init()
     pygame.mixer.music.load("audio/alien_danger.wav")
     pygame.mixer.music.play()
@@ -77,8 +82,23 @@ def play_emergency_sound():
         sleep(.25)
 
 
+def play_background_sound():
+    print("Starting background sound thread" )
+    t = threading.currentThread()
+    while getattr(t, "do_run", True):
+        print("Playing background sound")
+        pygame.mixer.init()
+        pygame.mixer.music.load("audio/buzzer.wav")
 
-def monitor(a):
+        while True:
+            pygame.mixer.music.play()
+            while pygame.mixer.music.get_busy() == True:
+                sleep(.25)
+    print( "Stopping background sound thread" )
+
+
+def monitor():
+    print("Starting gyroscope monitor")
     global gyro_x
     global gyro_y
     global gyro_z
@@ -88,7 +108,7 @@ def monitor(a):
         gyro_x = gyro_data['x']
         gyro_y = gyro_data['y']
         gyro_z = gyro_data['z']
-        print ("Gyro data is: " + str(gyro_x) + " " + str(gyro_y) + " " + str(gyro_z) )
+        #print ("Gyro data is: " + str(gyro_x) + " " + str(gyro_y) + " " + str(gyro_z) )
         sleep(0.6)
 
       
@@ -96,9 +116,19 @@ def monitor(a):
 # ===================================================================
 # Main program 
 # ===================================================================
-t = Thread(target=monitor, args=("foo",))
-t.start()
+
+# Start the giroscope and accelerometer monitoring
+giroThread = threading.Thread( target=monitor, args=() )
+giroThread.start()
+
+# Start the background sound
+bst = threading.Thread( target=play_background_sound, args=() )
+bst.start()
+
+# Reset all the LED's
 leds_reset()
+
+# Start "rest" mode by looping though some LED patterns continually 
 while True:
     leds_morph("#00ff84", 100.0)
     leds_morph("#ffffff", 100.0)
--- tests/audio_test.py
+++ tests/audio_test.py
@@ -1,7 +1,9 @@
 import time
 import pygame
 pygame.mixer.init()
-pygame.mixer.music.load("../audio/alien_danger.wav")
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy() == True:
+pygame.mixer.music.load("../audio/buzzer.wav")
+
+for count in range (5):
+  pygame.mixer.music.play()
+  while pygame.mixer.music.get_busy() == True:
     time.sleep(.25)
